8e38b03039242793652a7b4cd99d078fe82c13c8
Alung <alungsatrio12@gmail.com>
2025-11-17 15:18:48 +0700

    setting css

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.1.3"],
+};
+
 export default nextConfig;
