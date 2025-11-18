165e7a48df6af7ae8ced711e389b7efba39490a4
Alung <alungsatrio12@gmail.com>
2025-11-18 13:52:34 +0700

    change ide

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import ExploreBtn from "./components/ExploreBtn";
+
 const Page = () => {
   return (
     <div>
@@ -7,6 +9,15 @@ const Page = () => {
           The ultimate Hub for developers!
         </h1>
         <p className="text-center mt-5">This is a simple page component.</p>
+        <ExploreBtn />
+
+        <div className="mt-20 space-y-7">
+          <h3>Featured Events</h3>
+          <ul className="events">
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </section>
     </div>
   );
